fe0353bf3bfe7aa22d6ce27cbb63bece8e524f0d
Yoojae <yooflyhigh@gmail.com>
2020-06-23 17:15:29 +0900

    lab 13~16 cli 설치 및 구조 확인,

---
+++ lab13~/src/views/Home.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>This is home page</h1>
+    <!-- <KossieCoder title="home title" name="코지 코더"/> -->
+    <form>
+  </div>
+</template>
+
+<script>
+import KossieCoder from '@/components/KossieCoder.vue';
+export default {
+  components: {
+    KossieCoder
+  },
+  data(){
+    return{
+      name: 'Kossie Coder'
+    }
+  }
+}
+</script>
+
+<style scoped>
+h1{
+  color: red;
+}
+</style>
