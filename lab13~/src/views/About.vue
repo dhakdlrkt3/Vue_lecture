31c03d4b462f0943653c5c84c782485d1cb5de2c
Yoojae <yooflyhigh@gmail.com>
2020-06-23 19:49:09 +0900

    lab 18-1 slot 구현

--- lab13~/src/views/About.vue
+++ lab13~/src/views/About.vue
@@ -1,7 +1,19 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
-      <KossieCoder/>
+      <KossieCoder>
+        <!-- v-slot: 은 #으로 축약 가능 -->
+          <!-- 
+            이렇게 해도 됨
+            <template #header="props">
+            <p>header111 {{ props.kossie}}</p> -->
+          <template #header="{kossie}">
+            <p>header111 {{ kossie }}</p>
+          </template>
+          <template #default>
+            hello1
+          </template>
+      </KossieCoder>
   </div>
 </template>
 <script>
